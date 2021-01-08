bb1523aa7a2b47c3166e8bc6930414d6f66aab24
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-01-08 15:10:21 +0100

    update ft_strcmp function

--- ft_asm/ft_strcmp.s
+++ ft_asm/ft_strcmp.s
@@ -7,26 +7,26 @@ section .text
 
 _ft_strcmp:
 	xor 	rax, rax		; define rax to 0
-	jmp		compa		    ; call compa
+	jmp		_compare		; call compa
 
-compa:
-	mov		al, BYTE [rdi]	; store one byte (char) from arg0 in rdi
-	mov		bl, BYTE [rsi]	; store one byte (char) from arg1 in rsi
+_compare:
+	mov		al, BYTE [rdi]	; store one byte (char) in al register from arg0 in rdi
+	mov		bl, BYTE [rsi]	; store one byte (char) in bl register from arg1 in rsi
 
-	cmp		al, 0			; if we are not at the end of rdi (arg0)
-	je		exit			; jump to exit_end
+	cmp		al, 0			; check if we are at the end of rdi (arg0)
+	je		_exit			; if yes, jump to exit
 
-	cmp		bl, 0			; if we are not at the end of rdi (arg1)
-	je		exit			; jump to exit
+	cmp		bl, 0			; check if we are at the end of rdi (arg1)
+	je		_exit			; if yes, jump to exit
 
 	cmp 	al, bl			; compare al and bl
-	jne 	exit			; if the result of cmp is diferrent than 0 so the string are differents
+	jne 	_exit			; if the result of cmp is not equal 0 so the strings are differents
 
 	inc 	rdi				; increment the rdi pointer
 	inc 	rsi				; increment the rsi pointer
-	jmp 	compa			; if we are here it's because the chars of string are equals the we can continu
+	jmp 	_compare		; if we are here it's because the chars of string are equals the we can continu
 
-exit:
+_exit:
 	movzx	rax, al			; movzx ==> copy a register of inferior size in a bigger and fill the other bits with 0.
     movzx	rbx, bl			; same that previous but set it in rbx
     sub		rax, rbx		; stock the difference of rax and rbx in rax
